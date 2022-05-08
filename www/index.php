b001162a9023dd85d670b8436a48a1e00e53dae8
Hyldra Zolxy <hyldra.zolxy@outlook.fr>
2022-05-08 16:44:55 +0200

    Mobile design

---
+++ www/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <title>Hyldra Zolxy - Développeur Web en Freelance</title>
+        <link rel="stylesheet" href="ionicons.min.css" type="text/css" />
+        <link rel="stylesheet" href="style.global.css" type="text/css" />
+    </head>
+    <body>
+        <header>
+            <div id="logo">
+                <span>Hyldra</span>
+                <span>Zolxy</span>
+            </div>
+            <div id="menu">
+                <span class="ion-navicon-round"></span>
+            </div>
+        </header>
+        <main>
+            <section id="first" class="flex">
+                <h1>En développement</h1>
+                <p>Le site web est en cours de développement, merci de revenir plus tard 💖</p>
+                <span class="ion-chevron-down"></span>
+            </section>
+        </main>
+    </body>
+</html>
